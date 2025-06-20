1274fed16aa02c21acd8d5e1c124d8878c82d0d3
Abigail Kagoiza <151449310+Kagoiza@users.noreply.github.com>
2025-06-20 11:30:51 +0300

    Update App.js

--- AdminDashboard/src/App.js
+++ AdminDashboard/src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
 import AdminDashboard from './components/AdminDashboard';
+
 function App() {
-  return <AdminDashboard />;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="ml-60 p-6 bg-ictaGray min-h-screen w-full">
+        <AdminDashboard />
+      </div>
+    </div>
+  );
 }
+
 export default App;
